b8e060620b0ad3f47436e9b1f4a97543814d71fc
Zedb0T <89345505+Zedb0T@users.noreply.github.com>
2024-11-29 19:35:40 -0500

    init

--- PmdView/GameMain.cs
+++ PmdView/GameMain.cs
@@ -16,8 +16,10 @@ using System.Xml.Xsl;
 using RectpackSharp;
 using System.Linq.Expressions;
 
-namespace PmdView {
-	internal class GameMain : Game {
+namespace PmdView
+{
+	internal class GameMain : Game
+	{
 		GraphicsDeviceManager gdm;
 
 		ImGuiRenderer imRenderer;
@@ -26,6 +28,10 @@ namespace PmdView {
 		string pmdPath = string.Empty;
 		string tpfPath = string.Empty;
 		string pfPath = string.Empty;
+
+		string isoPath = Path.Combine(Environment.CurrentDirectory.Remove(Environment.CurrentDirectory.Length - 17), "ISO_DATA");
+		string plyDumpFolder = Path.Combine(Environment.CurrentDirectory.Remove(Environment.CurrentDirectory.Length - 17), "Extracted_PLY_Models");
+		XnaPmd mdl;
 		string pfDumpFolder = string.Empty;
 		bool pfDumpPrefixIndex = true;
 
@@ -41,37 +47,47 @@ namespace PmdView {
 
 		Vector2 vramOffset = new();
 
-		public void LoadTpf(string path) {
+		public void LoadTpf(string path)
+		{
 			path = path.Trim('"');
 			timBundle?.Dispose();
 			timBundle = null;
-			try {
+			try
+			{
 				timBundle = TimBundle.FromTpf(path, GraphicsDevice);
 				tpfPath = string.Empty;
-			} catch(Exception e) {
+			}
+			catch (Exception e)
+			{
 				lastException = e;
 				showErrorModal = true;
 			}
 		}
 
-		public void LoadMainPmd(string path) {
+		public void LoadMainPmd(string path)
+		{
 			path = path.Trim('"');
 			mainMdl?.Dispose();
 			mainMdl = null;
 			mdlFrame = 0;
-			try {
+			try
+			{
 				var pmd = Pmd.FromFile(path);
 				mainMdl = new(in pmd, GraphicsDevice);
 				pmdPath = string.Empty;
-			} catch (Exception e) {
+			}
+			catch (Exception e)
+			{
 				mainMdl?.Dispose();
 				lastException = e;
 				showErrorModal = true;
 			}
 		}
 
-		public GameMain() {
-			gdm = new(this) {
+		public GameMain()
+		{
+			gdm = new(this)
+			{
 				SynchronizeWithVerticalRetrace = true
 			};
 			IsMouseVisible = true;
@@ -90,10 +106,129 @@ namespace PmdView {
 		Exception lastException;
 		int pfSelectedItem = -1;
 
-		protected override void Draw(GameTime gameTime) {
+
+public void ExtractisotofolderPLY(string pfPath)
+{
+    isoPath = pfPath;
+    for (var i = 1; i <= 20; i++)
+    {
+        string stagePath = "STAGE" + i;
+
+        if (Directory.Exists(isoPath + "\\" + stagePath))
+        {
+            Console.Write(isoPath + "\\" + stagePath + " exists! \n");
+            tpfPath = isoPath.Trim('"');
+            tpfPath = isoPath + "\\" + stagePath + "\\STAGE.TPF";
+
+            timBundle?.Dispose();
+            timBundle = null;
+            try
+            {
+                timBundle = TimBundle.FromTpf(tpfPath, GraphicsDevice);
+                tpfPath = string.Empty;
+            }
+            catch (Exception e)
+            {
+                lastException = e;
+                showErrorModal = true;
+            }
+
+            pfPath = isoPath + "\\" + stagePath + "\\STAGE1.PPF";
+            pfPath = pfPath.Trim('"');
+            pf?.Dispose();
+            pfSelectedItem = -1;
+            try
+            {
+                pf = PackFile.FromFile(pfPath);
+            }
+            catch (Exception e)
+            {
+                pf = null;
+                lastException = e;
+                showErrorModal = true;
+            }
+
+            if (pf is not null)
+            {
+                Console.Write("pf is not null!");
+                for (var p = 0; p < pf.files.Count; p++)
+                {
+                    bool isSelected = pfSelectedItem == p;
+                    ImGui.PushID(p);
+
+                    // Dynamically check for stageXX.pmd
+                    for (var stageIndex = 1; stageIndex <= 20; stageIndex++)
+                    {
+                        string expectedFile = $"stage{stageIndex:00}.pmd";
+                        if (pf.files[p].filename.Equals(expectedFile, StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.Write($"{expectedFile} file was found! \n");
+                            pfSelectedItem = p;
+                            break;
+                        }
+                    }
+
+                    ImGui.PopID();
+                    if (isSelected)
+                    {
+                        ImGui.SetItemDefaultFocus();
+                    }
+                }
+
+                try
+                {
+                    mdlFrame = 0;
+                    mainMdl?.Dispose();
+                    Pmd pmd = Pmd.FromBytes(in pf.files[pfSelectedItem].data);
+                    mainMdl = new(in pmd, GraphicsDevice);
+                }
+                catch (Exception e)
+                {
+                    lastException = e;
+                    showErrorModal = true;
+                }
+            }
+
+            plyExportFolder = plyDumpFolder + "\\" + stagePath;
+
+            if (!Directory.Exists(plyExportFolder))
+            {
+                System.IO.Directory.CreateDirectory(plyExportFolder);
+            }
+
+            try
+            {
+                for (var x = 0; x < mainMdl?.pmd.frameVertices.GetLength(0); x++)
+                {
+                    string path = $"{plyExportFolder}{Path.DirectorySeparatorChar}Frame_{x}.ply";
+                    Pmd2Ply.WritePly(in mainMdl.pmd, path, in timBundle, x);
+                }
+
+                using (FileStream stream = new($"{plyExportFolder}{Path.DirectorySeparatorChar}Texture{i}.png", FileMode.Create))
+                {
+                    timBundle.texture.SaveAsPng(stream, timBundle.texture.Width, timBundle.texture.Height);
+                }
+            }
+            catch (Exception e)
+            {
+                lastException = e;
+                showErrorModal = true;
+            }
+        }
+        else
+        {
+            Console.Write("ERROR: " + pfPath + "\\" + stagePath + " does not exist! \n");
+        }
+    }
+}
+
+
+		protected override void Draw(GameTime gameTime)
+		{
 			GraphicsDevice.Clear(Color.Black);
 
-			if (timBundle?.texture is not null) {
+			if (timBundle?.texture is not null)
+			{
 				sbatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
 				sbatch.Draw(timBundle.texture, vramOffset, Color.White);
 				sbatch.End();
@@ -103,14 +238,15 @@ namespace PmdView {
 
 			//Vector3 cameraPosition = new Vector3(1130.0f, 130.0f, 1130.0f);
 			//Vector3 cameraTarget = new Vector3(0.0f, 0.0f, 0.0f); // Look back at the origin
-			
+
 			Vector3 cameraTarget = new Vector3(0.0f, 0, 0.0f); // Look back at the origin
 			Vector3 cameraPosition = new Vector3(
 				(float)Math.Cos(gameTime.TotalGameTime.TotalSeconds) * 100,
 				200,
 				(float)Math.Sin(gameTime.TotalGameTime.TotalSeconds) * 100);
 
-			if (canControlCam) {
+			if (canControlCam)
+			{
 				yaw -= io.MouseDelta.X;
 				pitch -= io.MouseDelta.Y;
 				pitch = Math.Clamp(pitch, -90, 90);
@@ -118,33 +254,44 @@ namespace PmdView {
 
 			Matrix rot = Matrix.CreateFromYawPitchRoll(MathHelper.ToRadians(yaw), MathHelper.ToRadians(pitch), 0f);
 			var keystate = Keyboard.GetState();
-			if (canControlCam) {
+			if (canControlCam)
+			{
 				float mult = keystate.IsKeyDown(Keys.LeftShift) ? 8 : 1;
-				if (keystate.IsKeyDown(Keys.W)) {
+				if (keystate.IsKeyDown(Keys.W))
+				{
 					camPos += rot.Forward * mult;
 				}
-				if (keystate.IsKeyDown(Keys.S)) {
+				if (keystate.IsKeyDown(Keys.S))
+				{
 					camPos += rot.Backward * mult;
 				}
-				if (keystate.IsKeyDown(Keys.A)) {
+				if (keystate.IsKeyDown(Keys.A))
+				{
 					camPos += rot.Left * mult;
 				}
-				if (keystate.IsKeyDown(Keys.D)) {
+				if (keystate.IsKeyDown(Keys.D))
+				{
 					camPos += rot.Right * mult;
 				}
-				if (keystate.IsKeyDown(Keys.E)) {
+				if (keystate.IsKeyDown(Keys.E))
+				{
 					camPos += rot.Up * mult;
 				}
-				if (keystate.IsKeyDown(Keys.Q)) {
+				if (keystate.IsKeyDown(Keys.Q))
+				{
 					camPos += rot.Down * mult;
 				}
 			}
-			if (keystate.IsKeyDown(Keys.Escape)) {
-				if (canToggleCam) {
+			if (keystate.IsKeyDown(Keys.Escape))
+			{
+				if (canToggleCam)
+				{
 					canControlCam = !canControlCam;
 					canToggleCam = false;
 				}
-			} else {
+			}
+			else
+			{
 				canToggleCam = true;
 			}
 
@@ -157,10 +304,14 @@ namespace PmdView {
 
 			imRenderer.BeforeLayout(gameTime);
 
-			if (timBundle is not null) {
-				try {
+			if (timBundle is not null)
+			{
+				try
+				{
 					mainMdl?.Draw(mdlFrame, world * sc, view, proj, GraphicsDevice, in timBundle);
-				} catch (Exception e) {
+				}
+				catch (Exception e)
+				{
 					lastException = e;
 					showErrorModal = true;
 					mainMdl?.Dispose();
@@ -168,68 +319,93 @@ namespace PmdView {
 				}
 			}
 
-			if (showErrorModal) {
+			if (showErrorModal)
+			{
 				ImGui.OpenPopup("Error");
 				showErrorModal = false;
 			}
 			bool dummy = true;
-			
-			if(ImGui.BeginPopupModal("Error", ref dummy, ImGuiWindowFlags.AlwaysAutoResize)) {
+
+			if (ImGui.BeginPopupModal("Error", ref dummy, ImGuiWindowFlags.AlwaysAutoResize))
+			{
 				ImGui.TextUnformatted($"ERROR:\n{lastException?.ToString() ?? "Something nasty's going on or my code sucks (likely case)"}");
-				if(ImGui.Button("OK")) {
+				if (ImGui.Button("OK"))
+				{
 					ImGui.CloseCurrentPopup();
 				}
 				ImGui.EndPopup();
 			}
 			ImGui.Begin("Main", ImGuiWindowFlags.NoSavedSettings | ImGuiWindowFlags.AlwaysAutoResize);
-			
+
 			ImGui.InputText("Export path", ref plyExportFolder, 512);
-			if (ImGui.Button("Export main model to ply")) {
-				try {
-					for (var i = 0; i < mainMdl?.pmd.frameVertices.GetLength(0); i++) {
+			if (ImGui.Button("Export main model to ply"))
+			{
+				try
+				{
+					for (var i = 0; i < mainMdl?.pmd.frameVertices.GetLength(0); i++)
+					{
 						string path = $"{plyExportFolder}{Path.DirectorySeparatorChar}Frame_{i}.ply";
 						Pmd2Ply.WritePly(in mainMdl.pmd, path, in timBundle, i);
 					}
 					using (FileStream stream = new($"{plyExportFolder}{Path.DirectorySeparatorChar}Texture.png", FileMode.Create))
 						timBundle.texture.SaveAsPng(stream, timBundle.texture.Width, timBundle.texture.Height);
-				} catch (Exception e) {
+				}
+				catch (Exception e)
+				{
 					lastException = e;
 					showErrorModal = true;
 				}
 			}
 
 			ImGui.TextUnformatted($"yaw {yaw} pitch {pitch}");
-			ImGui.SliderInt("Frame", ref mdlFrame, 0, mainMdl?.pmd.frameVertices.GetLength(0)-1 ?? 0);
+			ImGui.SliderInt("Frame", ref mdlFrame, 0, mainMdl?.pmd.frameVertices.GetLength(0) - 1 ?? 0);
 			var scaleNumerics = Shenanigans.XnaToNumerics(mdlScale);
 			ImGui.SliderFloat3("Model scale", ref scaleNumerics, -16, 16);
 			mdlScale = Shenanigans.NumericsToXna(scaleNumerics);
 
 			ImGui.InputText("Texture bundle path (TPF)", ref tpfPath, 512);
 			ImGui.SameLine();
-			if(ImGui.Button("Load")) {
+			if (ImGui.Button("Load"))
+			{
 				LoadTpf(tpfPath);
 			}
 			ImGui.InputText("Main model (PMD)", ref pmdPath, 512);
 			ImGui.SameLine();
-			if (ImGui.Button("Load##pmd")) {
+			if (ImGui.Button("Load##pmd"))
+			{
 				LoadMainPmd(pmdPath);
 			}
 
+			ImGui.InputText("PATH TO ISO_DATA", ref isoPath, 512);
+			ImGui.SameLine();
+			if (ImGui.Button("Extract all to PLY"))
+			{
+
+				ExtractisotofolderPLY(isoPath);
+			}
+
+
 			//////////////////////////////////////////////////
 
-			if (ImGui.CollapsingHeader("Object Visibility")) {
+			if (ImGui.CollapsingHeader("Object Visibility"))
+			{
 				ImGui.Indent();
-				if(ImGui.Button("Show all")) {
-					for (var i = 0; i < mainMdl?.objs.Length; i++) {
+				if (ImGui.Button("Show all"))
+				{
+					for (var i = 0; i < mainMdl?.objs.Length; i++)
+					{
 						mainMdl.objs[i].show = true;
 					}
 				}
-				for(var i = 0; i < mainMdl?.objs.Length; i++) {
+				for (var i = 0; i < mainMdl?.objs.Length; i++)
+				{
 					ImGui.PushID(i);
 					ImGui.Checkbox($"Object {i}", ref mainMdl.objs[i].show);
 					ImGui.SameLine();
-					if (ImGui.Button("Solo")) {
-						for(var j = 0; j < mainMdl.objs.Length; j++) {
+					if (ImGui.Button("Solo"))
+					{
+						for (var j = 0; j < mainMdl.objs.Length; j++)
+						{
 							mainMdl.objs[j].show = j == i;
 						}
 					}
@@ -239,32 +415,42 @@ namespace PmdView {
 			}
 
 			//////////////////////////////////////////////////
-			if(ImGui.CollapsingHeader("Packfile Viewer")) {
+			if (ImGui.CollapsingHeader("Packfile Viewer"))
+			{
 				ImGui.Indent();
 				ImGui.TextUnformatted("Path to packfile");
 				ImGui.InputText("##pfpath", ref pfPath, 512); ImGui.SameLine();
-				if (ImGui.Button("Load##pfloadbt")) {
+				if (ImGui.Button("Load##pfloadbt"))
+				{
 					pfPath = pfPath.Trim('"');
 					pf?.Dispose();
 					pfSelectedItem = -1;
-					try {
+					try
+					{
 						pf = PackFile.FromFile(pfPath);
-					} catch (Exception e) {
+					}
+					catch (Exception e)
+					{
 						pf = null;
 						lastException = e;
 						showErrorModal = true;
 					}
 				}
-				if (pf is not null) {
-					if (ImGui.BeginListBox("##pflistbox", new(200f, 200f))) {
-						for(var i = 0; i < pf.files.Count; i++) {
+				if (pf is not null)
+				{
+					if (ImGui.BeginListBox("##pflistbox", new(200f, 200f)))
+					{
+						for (var i = 0; i < pf.files.Count; i++)
+						{
 							bool isSelected = pfSelectedItem == i;
 							ImGui.PushID(i);
-							if (ImGui.Selectable(pf.files[i].filename, isSelected)) {
+							if (ImGui.Selectable(pf.files[i].filename, isSelected))
+							{
 								pfSelectedItem = i;
 							}
 							ImGui.PopID();
-							if(isSelected) {
+							if (isSelected)
+							{
 								ImGui.SetItemDefaultFocus();
 							}
 						}
@@ -272,27 +458,35 @@ namespace PmdView {
 					}
 					ImGui.SameLine();
 					ImGui.BeginGroup();
-					if(ImGui.Button("Load as model")) {
-						try {
+					if (ImGui.Button("Load as model"))
+					{
+						try
+						{
 							mdlFrame = 0;
 							mainMdl?.Dispose();
 							Pmd pmd = Pmd.FromBytes(in pf.files[pfSelectedItem].data);
 							mainMdl = new(in pmd, GraphicsDevice);
-						} catch(Exception e) {
+						}
+						catch (Exception e)
+						{
 							lastException = e;
 							showErrorModal = true;
 						}
 					}
 					ImGui.EndGroup();
 					ImGui.InputText("##pfdumppath", ref pfDumpFolder, 512); ImGui.SameLine();
-					if (ImGui.Button("Export files to folder")) {
-						try {
+					if (ImGui.Button("Export files to folder"))
+					{
+						try
+						{
 							pfDumpFolder = pfDumpFolder.Trim('"');
 							if (pfDumpFolder == string.Empty)
 								throw new Exception("HEY JACKASS!!!!!! You don't want to dump files in the root of the program.\nInsert the path to a folder.");
 							else
 								pf.Dump(pfDumpFolder, pfDumpPrefixIndex);
-						} catch (Exception e) {
+						}
+						catch (Exception e)
+						{
 							lastException = e;
 							showErrorModal = true;
 						}
@@ -307,18 +501,22 @@ namespace PmdView {
 
 			base.Draw(gameTime);
 		}
-		protected override void Update(GameTime gameTime) {
-			if (io.MouseDown[2]) {
+		protected override void Update(GameTime gameTime)
+		{
+			if (io.MouseDown[2])
+			{
 				vramOffset.X += io.MouseDelta.X;
 				vramOffset.Y += io.MouseDelta.Y;
 			}
 			base.Update(gameTime);
 		}
-		protected override void Initialize() {
-			
+		protected override void Initialize()
+		{
+
 			base.Initialize();
 		}
-		protected override void LoadContent() {
+		protected override void LoadContent()
+		{
 			sbatch = new(GraphicsDevice);
 			imRenderer = new(this);
 			imRenderer.RebuildFontAtlas();
